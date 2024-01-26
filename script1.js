12cfa4415d9bc6be41d9d121f27d7419d6684a4e
SamitSaha <samitsaha99@gmail.com>
2024-01-26 10:11:03 +0600

    updated

--- script1.js
+++ script1.js
@@ -3,4 +3,4 @@ let js = "amazing"
 if (js === "amazing") alert('JavaScript is FUN!')
 js = 'boring'
 if (js === "amazing") alert('JavaScript is FUN!')
-40 + 8 - 10 + 23
+console.log(40 + 8 - 10 + 23); 

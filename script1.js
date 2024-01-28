34f2a3133b0297c3f7a437ee832afebdab42cfc4
SamitSaha <samitsaha99@gmail.com>
2024-01-28 20:10:09 +0600

    updated

--- script1.js
+++ script1.js
@@ -3,4 +3,33 @@ let js = "amazing"
 if (js === "amazing") alert('JavaScript is FUN!')
 js = 'boring'
 if (js === "amazing") alert('JavaScript is FUN!')
-console.log(40 + 8 - 10 + 23); 
+console.log(40 + 8 - 10 + 23);
+
+let f = "Samit";
+console.log(f);
+console.log(f);
+console.log(f);
+
+let a = "a";
+let b = "v";
+let c = "c";
+let $d = "dollar can be first letter of a variable name ";
+
+
+let lowecase = "first letter cannot be uppercase.";
+let PI = 3.1416;
+
+true;
+let aa = "true";
+console.log(aa);
+
+console.log(typeof true);
+console.log(typeof aa);
+console.log(typeof 23);
+console.log(typeof 'Samit');
+
+aa = 'YES!';
+console.log(typeof aa);
+
+let ab;
+console.log(typeof ab);

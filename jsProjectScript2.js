e8d547d71ac2a9c4bbac3eb461b2264c2fc10044
SamitSaha <samitsaha99@gmail.com>
2024-02-26 20:51:13 +0600

    javascript project 2

---
+++ jsProjectScript2.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModel = function () {
+  console.log('Button Clicked!');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  modal.style.display = 'block';
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModel);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log('A key was pressed');
+  console.log(e);
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    console.log('ESC was pressed');
+    closeModal();
+  }
+});

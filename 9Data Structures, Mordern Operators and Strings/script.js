a4ed01dc876717165a78a2c932ad33adae4b7f3b
SamitSaha <samitsaha99@gmail.com>
2024-03-22 12:14:01 +0600

    updated

--- 9Data Structures, Mordern Operators and Strings/script.js
+++ 9Data Structures, Mordern Operators and Strings/script.js
@@ -240,5 +240,43 @@ if (resturant.orderPizza) {
   resturant.orderPizza('LOL', 'LALA');
 }
 
+// ///////////  Logical Assignment Operators ////////////////
+console.log('///////////  Logical Assignment Operators ////////////////');
+const g3 = resturant.numGuests ?? 10;
+console.log(g3);
+
 // ///////////  the nullish coalescing operator ////////////////
 console.log('the nullish coalescing operator');
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+const rest2 = {
+  name: 'la Piazza',
+  owner: 'Samit',
+};
+
+// OR assignment operator
+rest2.numGuests = rest1.numGuests || 10;
+rest1.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
+console.log(rest1);
+console.log(rest2);
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+console.log(rest1);
+console.log(rest2);
+
+// AND assignment operator
+rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+console.log(rest1);
+console.log(rest2);

caba32df933ec766aaa903e30928fdf9fcb71d82
SamitSaha <samitsaha99@gmail.com>
2024-03-25 01:21:12 +0600

    Update script.js

--- 9Data Structures, Mordern Operators and Strings/script.js
+++ 9Data Structures, Mordern Operators and Strings/script.js
@@ -387,3 +387,29 @@ rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
 console.log(rest.size);
 console.log(rest.get(arrr));
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'try again'],
+]);
+console.log(question);
+console.log(Object.entries(openingHours)); // convert objects to map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your Answer'));
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+console.log([...question]);
+console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
